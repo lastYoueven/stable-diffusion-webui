72bed3d25929f6fa718bc31187dbb807c133d7dc
貔貅 <598020642@qq.com>
2023-07-31 09:55:37 +0800

    extensions add

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS= --autolaunch --update-check --enable-insecure-extension-access --gradio-queue --xformers
 
 call webui.bat
